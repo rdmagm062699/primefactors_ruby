ac41a93abafd28818a4edfb9648b178a08774fd0
Rob Miller <robmiller@robs-mbp.hisc.local>
2016-07-13 13:24:18 -0500

    Add tests for 1 and 2

--- lib/primes.rb
+++ lib/primes.rb
@@ -1,5 +1,11 @@
 class Primes
    def factors(number)
-   	   return []
+   	   prime_factors = []
+
+   	   if number > 1
+   	   	   prime_factors << 2
+   	   end
+
+      return prime_factors
    end
 end
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -4,4 +4,12 @@ describe Primes do
    it 'will return an empty list for 0' do
    	   expect(Primes.new.factors(0)).to eq []
    end
+
+   it 'will return an empty list for 1' do
+       expect(Primes.new.factors(1)).to eq []
+   end
+
+   it 'will return [2] for 2' do
+   	   expect(Primes.new.factors(2)).to eq [2]
+   end
 end
